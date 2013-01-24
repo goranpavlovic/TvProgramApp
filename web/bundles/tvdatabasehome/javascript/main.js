89fe9354cf84a81a1a0b309d7698d67f002e30e1
root <pavgogi@gmail.com>
2013-01-24 09:10:53 +0100

    proradio facebook login

--- web/bundles/tvdatabasehome/javascript/main.js
+++ web/bundles/tvdatabasehome/javascript/main.js
@@ -1,16 +1,13 @@
 
-function setHeaderPlace()
+/*
+function onFbInit() 
 {
-	var header = document.getElementById('mainheader');
-	var headerContainer = document.getElementById('header');
-	var clntContHeight = headerContainer.clientHeight;
-	var clntHeight = header.clientHeight;
-	header.style.position = 'relative';
-	header.style.top = (Math.floor(clntContHeight / 2) 
-		- Math.floor( clntHeight / 2 ) ).toString().concat('px');
+    if (typeof(FB) != 'undefined' && FB != null ) 
+    {
+        FB.Event.subscribe('auth.statusChange', function(response) 
+        {
+            setTimeout(goLogIn, 500);
+        });
+    }
 }
-
-function mainInit()
-{
-	setHeaderPlace();
-}
+*/
