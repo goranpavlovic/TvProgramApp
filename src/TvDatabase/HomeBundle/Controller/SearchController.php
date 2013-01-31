1f794961cb6bbe3cab1a101aec8f021c916ff4d3
nikolamisic <nikola.misic@elfak.rs>
2013-01-31 16:18:43 +0100

    dodat filter za search

--- src/TvDatabase/HomeBundle/Controller/SearchController.php
+++ src/TvDatabase/HomeBundle/Controller/SearchController.php
@@ -58,4 +58,155 @@ class SearchController extends Controller
     	}
 
         }
+        
+        public function FilterAction($req)
+        {
+        	$types = $this->getDoctrine()->getRepository('AcmeStoreBundle:MetaEAVEntityType')->findAll();
+        	$televisions = $this->getDoctrine()->getRepository('AcmeStoreBundle:TVStation')->findAll();
+        	$filter = false;
+        	$bTypes = array();
+        	$tvs = array();
+        	
+        	$requested = '';
+        	if($this->getRequest()->get('broadcastTypes') || $this->getRequest()->get('televisions'))
+        	{
+        		$requested = $this->getRequest()->getQueryString();
+	        	$filters = explode('&', $requested);
+	        	
+	        	foreach($filters as $filter)
+	        	{
+	        		if(strstr($filter,'broadcastType'))
+	        		{
+	        			$trim = str_replace('broadcastType=', '', $filter);
+	        			$trim = str_replace('%20', ' ', $trim);
+	        			array_push($bTypes, $trim);
+	        			//$bTypes += $trim;
+	        		}
+	        		else if(strstr($filter,'televisions'))
+	        		{
+	        			$trim = str_replace('televisions=', '', $filter);
+	        			$trim = str_replace('%20', ' ', $trim);
+	        			array_push($tvs, $trim);
+	        			//$tvs += $trim + ', ';
+	        		}
+	        	}
+	        	$filter = true;
+        	}
+        	
+        	$con = mysql_connect('localhost','root','Praksa2012');
+        	mysql_select_db('Praksa2012');
+        	mysql_set_charset('utf8',$con);
+        	$query = '';
+        	if($filter)
+        	{
+        		$filterString = '';
+        		if(empty($bTypes)) //ako se traze svi broadcast tipovi
+        		{
+        			//$filterString = 't.TvName IN (';
+        			foreach ($tvs as $tv)
+        			{
+        				$filterString =  $filterString . ",'" . $tv . "'";
+        			}
+        			//$filterString = strstr($filterString,',');
+        			$pos = strpos( $filterString, ',' );
+        			if( $pos !== FALSE )
+        			{
+        				$filterString = substr_replace( $filterString, '', $pos, 1 );
+        			}
+        			$filterString = 't.TvName IN (' . $filterString . ')';
+        		}
+        		else if(empty($tvs)) //ako se traze sve emisije
+        		{
+        			//$filterString = 't.TvName IN (';
+        			foreach ($bTypes as $bt)
+        			{
+        				$filterString =  ',' . $filterString . "'" . $bt . "'";
+        			}
+        			//$filterString = strstr($filterString,',');
+        			$pos = strpos( $filterString, ',' );
+        			if( $pos !== FALSE )
+        			{
+        				$filterString = substr_replace( $filterString, '', $pos, 1 );
+        			}
+        			$filterString = 'me.EntityTypeName IN (' . $filterString . ')';
+        		}
+        		else //ako se filtrira po oba kriterijuma
+        		{
+        			//$filterString = 't.TvName IN (';
+        			foreach ($tvs as $tv)
+        			{
+        				$filterString =  ',' . $filterString . "'" . $tv . "'";
+        			}
+        			//$filterString = strstr($filterString,',');
+        			$pos = strpos( $filterString, ',' );
+        			if( $pos !== FALSE )
+        			{
+        				$filterString = substr_replace( $filterString, '', $pos, 1 );
+        			}
+        			$filterString = 't.TvName IN (' . $filterString . ')';
+        			
+        			//$filterString = 't.TvName IN (';
+        			//$filterString = $filterString . ' AND me.EntityTypeName IN (';
+        			$subfilterString = '';
+        			foreach ($bTypes as $bt)
+        			{
+        				$subfilterString =  $subfilterString . ",'" . $bt . "'";
+        			}
+        			//$filterString = strstr($filterString,',');
+        			$pos = strpos( $subfilterString, ',' );
+        			if( $pos !== FALSE )
+        			{
+        				$subfilterString = substr_replace( $subfilterString, '', $pos, 1 );
+        			}
+        			$subfilterString = 'me.EntityTypeName IN (' . $subfilterString . ')';
+        			
+        			$filterString = $filterString . 'AND ' . $subfilterString;
+        			
+        			
+        		}
+	        	$query = 'SELECT t.TvName, e.DateTime, a.Value, e.TvStation, e.EntityId, me.EntityTypeName
+	                            FROM EAVEntity e,EAVAttributeValue a, TVStation t, MetaEAVEntityType me
+	                        WHERE a.EntityId = e.EntityId 
+	        					AND e.TvStation = t.TvId
+	        					AND  me.EntityTypeId = e.EntityTypeId
+	        					AND ' . $filterString . '
+	        					AND MATCH(a.Value) AGAINST
+	                        ("' . $req . '" IN BOOLEAN MODE);';
+        	}
+        	else 
+        	{
+        		$query = 'SELECT t.TvName, e.DateTime, a.Value, e.TvStation, e.EntityId, me.EntityTypeName
+	                            FROM EAVEntity e,EAVAttributeValue a, TVStation t, MetaEAVEntityType me
+	                        WHERE a.EntityId = e.EntityId
+	        					AND e.TvStation = t.TvId
+	        					AND  me.EntityTypeId = e.EntityTypeId
+	        					AND MATCH(a.Value) AGAINST
+	                        ("' . $req . '" IN BOOLEAN MODE);';
+        	}
+        	
+        	if(!$result = mysql_query($query))
+        		return new Response('<html><body>query error:' . $query . '</body></html>');
+        	
+        	$return = array();
+        	while($row = mysql_fetch_array($result))
+        	{
+        		array_push($return, $row);
+        	}
+        	$locale = $this->getRequest()->getLocale();
+        	mysql_close($con);
+        	
+        	$locale = $this->getRequest()->getLocale();
+        	return $this->render('TvDatabaseHomeBundle:Default:filterpage.html.twig',
+        			array('_locale' => $locale,
+        					'search' => $req,
+        					'types' => $types,
+        					'televisions' => $televisions,
+        					'req' => $requested,
+        					'bTypes' => $bTypes,
+        					'tvs' => $tvs,
+        					'filter' => false,
+        					'results' => $return,
+        					'query' => $query		
+        			));
+        }
 }
